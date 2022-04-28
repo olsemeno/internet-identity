b3ca7936bc5f1b54aea983910d6d2bf0adb63226
Nicolas Mattia <nicolas@dfinity.org>
2022-04-28 12:11:55 +0200

    wip

--- .github/actions/file-size/test.sh
+++ .github/actions/file-size/test.sh
@@ -1,10 +1,42 @@
 #!/usr/bin/env bash
 
-echo this is the test!
+set -euo pipefail
 
-echo "file: '$INPUT_FILE'"
-echo "ref: $INPUT_REF"
+filename=${INPUT_FILE:?"No filename provided"}
+name=${INPUT_NAME:-$filename}
+ref=${INPUT_REF:?"No ref provided"}
 
-size="$(wc -c <"$INPUT_FILE")"
+size="$(wc -c <"$filename" | xargs)"
 
 echo "::set-output name=size::$size"
+
+JQ_FILTER=$(cat <<-'END'
+{ name: $name,
+  filename: $filename,
+  size_bytes: $size,
+  type: "file-size",
+}
+END
+)
+
+jq_args=(
+    --arg name "$name"
+    --arg filename "$INPUT_FILE"
+    --arg size "$size"
+)
+
+out_file=$(mktemp)
+jq "${jq_args[@]}" "$JQ_FILTER" -n -c -M >"$out_file"
+
+>&2 echo "JSON output:"
+>&2 cat "$out_file"
+
+logged() {
+    ( set -x && "$@" )
+}
+
+logged git fetch origin "refs/$ref:refs/$ref" || echo "could not fetch $ref, assuming doesn't exist yet"
+logged git notes append --file "$out_file"
+rm "$out_file"
+
+logged git push origin "refs/$ref"
