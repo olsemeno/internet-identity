d92befc044e9efe431faf03febf1aa024d3798a7
Nicolas Mattia <nicolas@dfinity.org>
2022-04-28 11:22:43 +0200

    wip

---
+++ .github/actions/file-size/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo this is the test!
+
+echo "file: $INPUT_FILES"
+echo "ref: $INPUT_REF"
+
+while IFS= read -r file; do
+    echo "... $file ..."
+    wc -c <"$file"
+done <<< "$INTPUT_FILES"
