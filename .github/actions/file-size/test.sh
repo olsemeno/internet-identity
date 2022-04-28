9e69ce597259f4feee3a67b24aa23c80ff075674
Nicolas Mattia <nicolas@dfinity.org>
2022-04-28 11:41:49 +0200

    wip

--- .github/actions/file-size/test.sh
+++ .github/actions/file-size/test.sh
@@ -2,10 +2,9 @@
 
 echo this is the test!
 
-echo "file: '$INPUT_FILES'"
+echo "file: '$INPUT_FILE'"
 echo "ref: $INPUT_REF"
 
-while IFS= read -r file; do
-    echo "... $file ..."
-    wc -c <"$file"
-done < <(printf '%s\n' "$INPUT_FILES")
+size="$(wc -c <"$INPUT_FILE")"
+
+echo "::set-output name=size::$size"
