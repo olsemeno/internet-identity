b567226e402642bc4c957b5aa63dbe70d793e89f
Nicolas Mattia <nicolas@dfinity.org>
2022-04-28 12:29:26 +0200

    wip

--- .github/actions/file-size/test.sh
+++ .github/actions/file-size/test.sh
@@ -11,6 +11,7 @@ echo "SAVE?? $save"
 
 size="$(wc -c <"$filename" | xargs)"
 
+>&2 echo "Size: $size"
 echo "::set-output name=size::$size"
 
 JQ_FILTER=$(cat <<-'END'
@@ -38,11 +39,15 @@ logged() {
     ( set -x && "$@" )
 }
 
-git config user.name "file-size action"
-git config user.email "<>"
+if [ "$save" = "true" ]
+then
+    >&2 echo "Saving size to git note"
+    git config user.name "file-size action"
+    git config user.email "<>"
 
-logged git fetch origin "refs/$ref:refs/$ref" || echo "could not fetch $ref, assuming doesn't exist yet"
-logged git notes append --file "$out_file"
-rm "$out_file"
+    logged git fetch origin "refs/$ref:refs/$ref" || echo "could not fetch $ref, assuming doesn't exist yet"
+    logged git notes --ref "$ref" append --file "$out_file"
+    rm "$out_file"
 
-logged git push origin "refs/$ref"
+    logged git push origin "refs/$ref"
+fi
