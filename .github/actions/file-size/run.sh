a2b384624f3f7b75181778701ae08ab44e17dc00
Nicolas Mattia <nicolas@dfinity.org>
2022-04-28 14:54:46 +0200

    wip

--- .github/actions/file-size/run.sh
+++ .github/actions/file-size/run.sh
@@ -5,38 +5,12 @@ set -euo pipefail
 filename=${INPUT_FILE:?"No filename provided"}
 save=${INPUT_SAVE}
 name="$filename"
-ref="notes/file-size"
+ref="notes/file-size/$name"
 
 size="$(wc -c <"$filename" | xargs)"
 
 >&2 echo "Size: $size"
 echo "::set-output name=size::$size"
-
-JQ_FILTER=$(cat <<-'END'
-{ name: $name,
-  filename: $filename,
-  size_bytes: $size,
-  type: "file-size",
-}
-END
-)
-
-jq_args=(
-    # NOTE: can potentially get filter from action input
-    --arg name "$name"
-    --arg filename "$filename"
-    --arg size "$size"
-
-    --null-input # TODO
-    --compact-output # TODO
-    --monochrome-output # TODO
-)
-
-out_file=$(mktemp)
-jq "${jq_args[@]}" "$JQ_FILTER" >"$out_file"
-
->&2 echo "JSON output:"
->&2 cat "$out_file"
 
 logged() {
     ( set -x && "$@" )
@@ -49,8 +23,7 @@ then
     git config user.email "<>"
 
     logged git fetch origin "refs/$ref:refs/$ref" || echo "could not fetch $ref, assuming doesn't exist yet"
-    logged git notes --ref "$ref" append --file "$out_file"
-    rm "$out_file"
+    logged git notes --ref "$ref" append -m "$size"
 
     logged git push origin "refs/$ref"
 fi
