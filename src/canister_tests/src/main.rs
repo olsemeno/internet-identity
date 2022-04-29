7d1fad594308b248316eae6e09377159a7f69717
Nicolas Mattia <nicolas@dfinity.org>
2022-04-29 16:47:07 +0200

    wip

---
+++ src/canister_tests/src/main.rs
@@ -0,0 +1,98 @@
+use ic_cdk::export::candid::utils::{decode_args, encode_args, ArgumentDecoder, ArgumentEncoder};
+use ic_cdk::export::candid::{parser::value::IDLValue, IDLArgs};
+use ic_state_machine_tests::{CanisterId, StateMachine, UserError, WasmResult};
+use serde_bytes::ByteBuf;
+
+/*
+#[derive(Debug)]
+enum CallError {
+    Reject(String),
+    UserError(UserError),
+}
+
+/// A helper function that we use to implement both [`call_candid`] and
+/// [`query_candid`].
+fn with_candid<Input, Output>(
+    input: Input,
+    f: impl FnOnce(Vec<u8>) -> Result<WasmResult, UserError>,
+) -> Result<Output, CallError>
+where
+    Input: ArgumentEncoder,
+    Output: for<'a> ArgumentDecoder<'a>,
+{
+    let in_bytes = encode_args(input).expect("failed to encode args");
+    match f(in_bytes) {
+        Ok(WasmResult::Reply(out_bytes)) => Ok(decode_args(&out_bytes).unwrap_or_else(|e| {
+            panic!(
+                "Failed to decode bytes {:?} as candid type: {}",
+                std::any::type_name::<Output>(),
+                e
+            )
+        })),
+        Ok(WasmResult::Reject(message)) => Err(CallError::Reject(message)),
+        Err(user_error) => Err(CallError::UserError(user_error)),
+    }
+}
+
+/// Call a canister candid method.
+fn call_candid<Input, Output>(
+    env: &StateMachine,
+    canister_id: CanisterId,
+    method: &str,
+    input: Input,
+) -> Result<Output, CallError>
+where
+    Input: ArgumentEncoder,
+    Output: for<'a> ArgumentDecoder<'a>,
+{
+    with_candid(input, |bytes| {
+        env.execute_ingress(canister_id, method, bytes)
+    })
+}
+
+/// Query a canister candid method.
+fn query_candid<Input, Output>(
+    env: &StateMachine,
+    canister_id: CanisterId,
+    method: &str,
+    input: Input,
+) -> Result<Output, CallError>
+where
+    Input: ArgumentEncoder,
+    Output: for<'a> ArgumentDecoder<'a>,
+{
+    with_candid(input, |bytes| env.query(canister_id, method, bytes))
+}
+*/
+
+/// Checks that a canister that uses [`ic_cdk::storage::stable_store`]
+/// and [`ic_cdk::storage::stable_restore`] functions can keep its data
+/// across upgrades.
+#[test]
+fn test_storage_roundtrip() {
+    let env = StateMachine::new();
+    let kv_store_wasm =  std::fs::read("../../internet_identity.wasm").unwrap();
+
+    let nulls = vec![IDLValue::Null; 1];
+    let args = IDLArgs::new(&nulls);
+    let byts = args.to_bytes().unwrap();
+    let canister_id = env
+        .install_canister(kv_store_wasm.clone(), byts, None)
+        .unwrap();
+
+    /*
+    let () = call_candid(&env, canister_id, "insert", (&"candid", &b"did"))
+        .expect("failed to insert 'candid'");
+    */
+
+    /*
+    env.upgrade_canister(canister_id, kv_store_wasm, vec![])
+        .expect("failed to upgrade the simple-kv-store canister");
+    */
+
+    /*
+    let (result,): (Option<ByteBuf>,) =
+        query_candid(&env, canister_id, "lookup", (&"candid",)).expect("failed to lookup 'candid'");
+    assert_eq!(result, Some(ByteBuf::from(b"did".to_vec())));
+    */
+}
